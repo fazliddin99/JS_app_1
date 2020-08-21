e3f00ff59295a4984b7e6b0b7213aabab26d0c88
Fazliddin <jabboroff1999@ya.ru>
2020-08-21 22:31:21 +0500

    Completed second step

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const numberOfFilms = +prompt("How many films did you watch?", "");
+'use strict';
+
+const numberOfFilms = prompt("How many films did you watch?", "");
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -6,14 +8,59 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Your favorite film", ""),
+          b = prompt("Rate it", "");
+
+    if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+        console.log('done');
+        personalMovieDB.movies[a] = b;
+    } else {
+        console.log('error');
+        i--;
+    } 
 }
 
-const a = prompt("Your favorite film", ""),
-      b = prompt("Rate it", ""),
-      c = prompt("Your favorite film", ""),
-      d = prompt("Rate it", "");
+// let i = 0;
+// while (i < 2) {
+//    i++; 
+//    const a = prompt("Your favorite film", ""),
+//           b = prompt("Rate it", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+//     if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+//         console.log('done');
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         console.log('error');
+//         i--;
+//     }  
+// }
+
+// let i = 0;
+// do {
+//     i++;
+//     const a = prompt("Your favorite film", ""),
+//           b = prompt("Rate it", "");
+
+//     if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+//         console.log('done');
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         console.log('error');
+//         i--;
+//     } 
+// } while (i < 2);
+ 
+if (personalMovieDB.count < 10) {
+    console.log('posmotreno dovolno malo filmov');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('vi klassicheskiy zritel');
+} else if (personalMovieDB.count >= 30) {
+    console.log('vi kinoman');
+} else {
+    console.log('proizoshlo oshibka');
+}
 
 console.log(personalMovieDB);
